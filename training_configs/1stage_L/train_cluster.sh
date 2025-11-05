59b7335552c1a24b0c2633fd8760a10eb2e312f6
Johannes Wesch <upygb@student.kit.edu>
2025-11-05 12:02:28 +0100

    chore: reduce job time to 20 hours

--- training_configs/1stage_L/train_cluster.sh
+++ training_configs/1stage_L/train_cluster.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=hnet-1stage-L
 #SBATCH --partition=gpu_h100_il
 #SBATCH --mem=510000mb
-#SBATCH --time=48:00:00
+#SBATCH --time=20:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4
